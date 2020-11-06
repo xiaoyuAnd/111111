fa9f7b1f5dd4be1c9d207081a8c2274788f4cb6e
xiaoyuAnd <yu11040921@163.com>
2020-11-06 17:03:07 +0800

    icon组件完成

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,31 @@
 <template>
 <div>
     <div id="nav">
-        <router-link to="/money">记账</router-link> |
-        <router-link to="/labels">标签</router-link> |
-        <router-link to="/statistics">统计</router-link>
+        <router-link to="/money">
+            <Icon name='money' />
+            记账
+        </router-link> |
+        <router-link to="/labels">
+            <Icon name='label' />
+            标签
+        </router-link> |
+        <router-link to="/statistics">
+            <Icon name='Statistics' />
+            统计
+        </router-link>
     </div>
 </div>
 </template>
 
-<script>
+<script lang="ts">
+// import x from '../assets/icons/label.svg';
+// import y from '../assets/icons/money.svg';
+// import z from '../assets/icons/Statistics.svg';
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// //一个一个引入
+
 export default {
 
 }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+/* eslint-disable */
+//鸵鸟做法  禁止eslink提示  谷歌搜eslink ignore whole file
+
+const path = require('path')
 module.exports = {
-  lintOnSave: false
+    lintOnSave: false,
+    chainWebpack: config => {
+        const dir = path.resolve(__dirname, 'src/assets/icons')
+        config.module
+            .rule('svg-sprite')
+            .test(/\.svg$/)
+            .include.add(dir).end() //只包含icons目录
+            .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false }).end() //extract:false是不需要解析一个文件出来
+        config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
+        config.module.rule('svg').exclude.add(dir) //其他SVG loader排除icons目录
+    }
 }
