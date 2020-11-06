55e2f8dc31f2eb3b0df2a462b9cde6e5df7acb77
xiaoyuAnd <yu11040921@163.com>
2020-11-06 17:58:06 +0800

    nav导航栏样式完成

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,15 @@
     box-sizing: border-box;
 }
 
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
     <div id="nav">
-        <router-link to="/money">
+        <router-link to="/money" class="item">
             <Icon name='money' />
             记账
-        </router-link> |
-        <router-link to="/labels">
+        </router-link>
+        <router-link to="/labels" class="item">
             <Icon name='label' />
             标签
-        </router-link> |
-        <router-link to="/statistics">
+        </router-link>
+        <router-link to="/statistics" class="item">
             <Icon name='Statistics' />
             统计
         </router-link>
@@ -33,6 +33,27 @@ export default {
 
 <style lang="scss" scoped>
 #nav {
-    border: 1px solid red
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+
+    >.item {
+        padding: 2px 0;
+        width: 33.33333%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        .icon {
+            width: 32px;
+            height: 32px;
+        }
+    }
+
+    >.item.selected {
+        color: red;
+    }
 }
 </style>
