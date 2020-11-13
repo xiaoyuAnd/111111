8a05da2b5e1a2e5b7047f9162fda11d9ac1a8565
xiaoyuAnd <yu11040921@163.com>
2020-11-13 09:58:26 +0800

    收集四个组件的数据在record里

---
+++ src/components/Moneys/Tags.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="tags">
+    <div class="new">
+        <button @click="create">新增标签</button>
+    </div>
+    <ul class="current">
+        <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)" :class="{selected: selectedTags.indexOf(tag)>=0}">{{tag}}</li>
+    </ul>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {
+    Component,
+    Prop
+} from 'vue-property-decorator'
+@Component
+export default class Tags extends Vue {
+    @Prop() dataSource: string[] | undefined
+    selectedTags: string[] = [];
+    toggle(tag: string) {
+        const index = this.selectedTags.indexOf(tag)
+        if (index >= 0) {
+            this.selectedTags.splice(index, 1)
+        } else {
+            this.selectedTags.push(tag)
+        }
+        this.$emit('updata:selected', this.selectedTags)
+    }
+    create() {
+        const name = window.prompt("请输入标签名");
+        if (name === '') {
+            window.alert('标签名不能为空');
+        } else if (this.dataSource) {
+            if(name === ''){
+                return this.$emit('update:dataSource',this.dataSource);
+            } else{
+                this.$emit('update:dataSource',[...this.dataSource, name]);
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+
+    >.current {
+        display: flex;
+        flex-wrap: wrap;
+
+        >li {
+            $bg: #d9d9d9;
+            background: $bg;
+            $h: 24px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 16px;
+            margin-right: 12px;
+            margin-top: 4px;
+
+            &.selected {
+                background: darken($bg, 50%);
+                color: #fff
+            }
+        }
+    }
+
+    >.new {
+        padding-top: 16px;
+
+        button {
+            background: transparent;
+            border: none;
+            color: #999;
+            border-bottom: 1px solid;
+            padding: 0 4px;
+        }
+    }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,62 @@
 <template>
 <Layout class-prefix="layout">
-    <NumberPad />
-    <Types />
-    <Notes />
-    <Tags />
+    {{record}}
+    <NumberPad :value.sync='record.amount' @updata:value="onUpdataAmount" />
+    <Types :value.sync='record.types'  />
+    <Notes @update:value="onUpdateNotes" />
+    <Tags :data-source.sync="tags" @updata:selected='onUpdataTags' />
 </Layout>
 </template>
 
 <script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
-export default {
+import Types from '@/components/Moneys/Types.vue';
+import Notes from '@/components/Moneys/Notes.vue';
+import Tags from '@/components/Moneys/Tags.vue';
+import NumberPad from '@/components/Moneys/NumberPad.vue';
+
+import Vue from 'vue'
+import {
+    Component
+} from 'vue-property-decorator';
+
+type Record = {
+    tags: string[];
+    notes: string;
+    types: string;
+    amount: number;
+}
+
+@Component({
     components: {
         Tags,
         Notes,
         Types,
         NumberPad
-    },
-};
+    }
+})
+export default class Money extends Vue {
+    tags = ['衣', '食', '住', '行', '医疗', '购物'];
+    record: Record = {
+        tags: [],
+        notes: '',
+        types: '-',
+        amount: 10
+    }
+    onUpdataTags(value: string[]) {
+        this.record.tags = value
+    }
+    onUpdateNotes(value: string) {
+        this.record.notes = value;
+    }
+    onUpdataTypes(value: string) {
+        this.record.types = value
+
+    }
+    onUpdataAmount(value: string) {
+        this.record.amount = parseFloat(value)
+
+    }
+}
 </script>
 
 <style lang="scss">
